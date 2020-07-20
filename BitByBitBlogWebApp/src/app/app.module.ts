b189d91c47090b8bd50e7b59e39f85ec674a02d3
alvaromon <alvaro.montoya@knights.ucf.edu>
2020-07-20 15:47:12 -0400

    update

--- BitByBitBlogWebApp/src/app/app.module.ts
+++ BitByBitBlogWebApp/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { BlogPreviewComponent } from './blog-preview/blog-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    BlogPreviewComponent
   ],
   imports: [
     BrowserModule,
